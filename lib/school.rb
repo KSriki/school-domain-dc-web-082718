119494fcb9ca06a069e638f816d9e6427a47f7b4
Srikant Kumar Kalaputapu <sriki@Srikants-MacBook-Pro.local>
2018-08-30 10:55:34 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+
+    attr_reader :roster,:name
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(students,grade)
+        # puts students
+        if @roster.has_key?(grade)
+            @roster[grade] += students.split("\n")
+        else
+            @roster[grade] = [students]
+        end
+    end
+
+    def grade(grade)
+        return @roster[grade]
+    end
+
+    def sort
+        @roster = Hash[@roster.sort.map {|a,b| [a,b.sort]}]
+    end
+end
